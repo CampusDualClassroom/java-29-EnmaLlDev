ab4ed1bec20479788f8d77f4d7bdd857079458d9
EnmaLlDev <enmanuel.lledo.1998@gmail.com>
2025-05-27 08:32:18 +0200

    Resuelto!

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -2,6 +2,15 @@ package com.campusdual.classroom;
 
 public class Exercise29 {
     public static void main(String[] args) {
+        try {
+            System.out.println("Instantiating the class...");
+            Thread.sleep(1000);
+            System.out.println("After thread!");
+        } catch (Exception e) {
+            // Catch any exception that occurs during instantiation
+            System.out.println("An error occurred: " + e.getMessage());
+        }
 
     }
+
 }
